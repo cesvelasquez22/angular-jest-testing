32a9f74c7299e3da1a43ee2062d1a281b181140a
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-04-03 17:11:40 -0600

    + Test father component

--- tests/basic/father/father.component.spec.ts
+++ tests/basic/father/father.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FatherComponent } from '../../../src/app/basic/father/father.component';
+import { FatherSonComponent } from '../../../src/app/basic/father-son/father-son.component';
+import { By } from '@angular/platform-browser';
 
 describe('FatherComponent', () => {
   let component: FatherComponent;
   let fixture: ComponentFixture<FatherComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FatherComponent ]
+      declarations: [ FatherComponent, FatherSonComponent ]
     })
     .compileComponents();
   });
@@ -17,9 +20,46 @@ describe('FatherComponent', () => {
     fixture = TestBed.createComponent(FatherComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
-  it('should create', () => {
+ test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('should match with snapshot', () => {
+    expect(compiled).toMatchSnapshot();
+  });
+
+  test('should set client with onSetClient method', () => {
+    component.onSetClient('Cesar');
+    fixture.detectChanges();
+
+    const codeDivParent = compiled.querySelector('.mt-2');
+    expect(codeDivParent?.textContent).toContain('"name"');
+    expect(codeDivParent?.textContent).toContain('"Cesar"');
+  });
+
+  test('should delete client if father son emit onDeleteClient event', () => {
+    component.client = { id: 1, name: 'Cesar' };
+    fixture.detectChanges();
+
+    const sonDebugElement = fixture.debugElement.query(By.directive(FatherSonComponent));
+    const sonComponent: FatherSonComponent = sonDebugElement.componentInstance;
+    
+    sonComponent.onDeleteClient.emit();
+    expect(component.client).toBe(undefined);
+  });
+
+  test('should update client if father son emit onClientUpdated event', () => {
+    component.client = { id: 1, name: 'Cesar' };
+    fixture.detectChanges();
+
+    const sonDebugElement = fixture.debugElement.query(By.directive(FatherSonComponent));
+    const sonComponent: FatherSonComponent = sonDebugElement.componentInstance;
+    
+    // sonComponent.onChange(10); Another possible method that emits the event
+    sonComponent.onClientUpdated.emit({ id: 10, name: 'Pedro' });
+    expect(component.client).toEqual({ id: 10, name: 'Pedro' });
   });
 });
